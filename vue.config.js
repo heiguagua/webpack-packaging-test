452af93830af3e9f04b8ef30490fc7727f00e4d6
xulan <xulan@chinawiserv.com>
2020-04-20 09:48:40 +0800

    modify: 修复eslint检查错误

--- vue.config.js
+++ vue.config.js
@@ -1,36 +1,62 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 // vue.config.js
-module.exports = {
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
+module.exports = {
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionWebpackPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: productionGzipExtensions,
+            threshold: 2048,
+            minRatio: 0.8
+          })
+        ]
+      }
+    }
+  },
   // 打包不生成sourcemap
   productionSourceMap: false,
-    chainWebpack: config => {
-      // 移除 prefetch 插件
+  chainWebpack: config => {
+    // 移除 prefetch 插件
     // config.plugins.delete('prefetch');
-    
-        config.module
-          .rule("eslint")
-          .use("eslint-loader")
-          .loader("eslint-loader")
-          .tap(options => {
-            options.fix = true;
-            return options;
-          });
-          
-          config.optimization.splitChunks({
-            chunks: 'all',
-            maxInitialRequests: Infinity,
-            minSize: 300000, // 依赖包超过300000bit将被单独打包
-            automaticNameDelimiter:'-',
-            // cacheGroups: {
-            //   vendor: {
-            //     test: /[\\/]node_modules[\\/]/,
-            //     name(module) {
-            //       const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            //       return `chunk.${packageName.replace('@', '')}`;
-            //     },
-            //     priority:10
-            //   }
-            // }
-          })
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .loader('eslint-loader')
+      .tap(options => {
+        options.fix = true
+        return options
+      })
+    config.optimization.splitChunks({
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 300000, // 依赖包超过300000bit将被单独打包
+      automaticNameDelimiter: '-',
+      cacheGroups: {
+        libs: {
+          name: 'chunk-libs',
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: 'initial' // only package third parties that are initially dependent
+        },
+        elementUI: {
+          name: 'chunk-elementUI', // split elementUI into a single package
+          priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+          test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+        }
+        // commons: {
+        //   name: 'chunk-commons',
+        //   test: resolve('src/components'), // can customize your rules
+        //   minChunks: 3, //  minimum common number
+        //   priority: 5,
+        //   reuseExistingChunk: true
+        // }
       }
+    })
+  }
 }
